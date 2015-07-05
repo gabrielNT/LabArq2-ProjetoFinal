01ead564827b5caf86ef5ca7eba7bc0e5f4d3fec
gabrielNT <gabrielNT@live.com>
2015-07-05 20:14:31 -0300

    SSE - Versões Finais
    Versões finais do algoritmo otimizados com o conjunto de instruções sse.

--- mandelbrotSSE_iX.c
+++ mandelbrotSSE_iX.c
@@ -10,88 +10,101 @@ http://en.wikipedia.org/wiki/Image:Color_complex_plot.jpg
 create 24 bit color graphic file ,  portable pixmap file = PPM
 see http://en.wikipedia.org/wiki/Portable_pixmap
 to see the file use external application ( graphic viewer)
+-------------------------------------------------
+3.Versao otimizada com inline assembly, com o conjunto de instrucoes SSE4_2. Essa versao
+paralelizou o loop em iX.
 */
 #include <stdio.h>
 #include <math.h>
-#include <time.h>
 #include <xmmintrin.h>
 
 int main()
 {
-	// Variaveis para medir o tempo
-	clock_t begin, end;
-	double time_spent;
-
-	begin = clock();
-
 	/* screen ( integer) coordinate */
 	int iX, iY;
 	const int iXmax = 16384;
 	const int iYmax = 16384;
+
 	/* world ( double) coordinate = parameter plane*/
-	float Cx, _Cy[4];
+	// A variavel Cx foi extendida para um vetor, para aproveitar o uso dos registradores sse
+	float _Cx[4], Cy;
 	const float CxMin = -2.5;
 	const float CxMax = 1.5;
 	const float CyMin = -2.0;
 	const float CyMax = 2.0;
-	/* */
 	float PixelWidth = (CxMax - CxMin) / iXmax;
 	float PixelHeight = (CyMax - CyMin) / iYmax;
+
 	/* color component ( R or G or B) is coded from 0 to 255 */
 	/* it is 24 bit color RGB file */
 	const int MaxColorComponentValue = 255;
 	FILE * fp;
-	char *filename = "mandelbrot.ppm";
+	char *filename = "_simd_sse_iX.ppm";
 	static unsigned char color[3];
+
+	// Foi criado um vetor auxiliar onde sera feita parte do calculo de color[]
+	int vetorCoresAux[4];
+
 	/* Z=Zx+Zy*i  ;   Z0 = 0 */
-	double Zx, Zy;
-	double Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
-	/*  */
-	int Iteration;
+	float Zx, Zy;
+	float Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
+
+	// A variavel Iteration foi extendida para um vetor, para aproveitar o uso dos registradores sse
+	int Iteration[4];
 	const int IterationMax = 256;
+
 	/* bail-out value , radius of circle ;  */
-	const double EscapeRadius = 2;
-	double ER2 = EscapeRadius*EscapeRadius;
+	const float EscapeRadius = 2;
+	float ER2 = EscapeRadius*EscapeRadius;
+
 	/*create new file,give it a name and open it in binary mode  */
 	fp = fopen(filename, "wb"); /* b -  binary mode */
+
 	/*write ASCII header to the file*/
 	fprintf(fp, "P6\n %d\n %d\n %d\n", iXmax, iYmax, MaxColorComponentValue);
-	
-	__m128 PixelHeight128 = _mm_set1_ps(PixelHeight);
-	__m128 CyMin128 = _mm_set1_ps(CyMin);
+
+	// Gera uma quatro palavras de 32 bits, com valor PixelWidth por meio de funcoes intrinsecas
+	__m128 PixelWidth128 = _mm_set1_ps(PixelWidth);
+	__m128 CxMin128 = _mm_set1_ps(CxMin);
 
 	/* compute and write image data bytes to the file*/
-	for (iY = 0; iY<iYmax/4; iY++)
+	for (iY = 0; iY<iYmax; iY++)
 	{
-		float simdIy[4];
-		for (int i = 0; i < 4; i++)
-			simdIy[i] = iY * 4.0 + i;
+		Cy = CyMin + iY*PixelHeight;
 
-		_asm{
-			movups xmm5, simdIy
-			mulps xmm5, PixelHeight128
-			addps xmm5, CyMin128    // xmm5 = CyMin + iY*PixelWidth
-			movups _Cy, xmm5
-		}
+		if (fabs(Cy)< PixelHeight / 2) Cy = 0.0; /* Main antenna */
+		
+		// Loop paralelizado(são feitas 4 iteracoes simultaneamente)
+		for (iX = 0; iX<iXmax / 4; iX++)
+		{
+			// Gera os indices e coloca em simdIx
+			float simdIx[4];
+			for (int i = 0; i < 4; i++)
+				simdIx[i] = iX * 4.0 + i;
 
-		for (int i = 0; i < 4; i++){
-			if (fabs(_Cy[i]) < PixelHeight / 2) _Cy[i] = 0.0; /* Main antenna */
-			for (iX = 0; iX < iXmax; iX++)
-			{
-				Cx = CxMin + iX*PixelWidth;
-				/* initial value of orbit = critical point Z= 0 */
-				Zx = 0.0;
-				Zy = 0.0;
-				Zx2 = Zx*Zx;
-				Zy2 = Zy*Zy;
-				/* */
+			_asm{
+				movups xmm5, simdIx
+					mulps xmm5, PixelWidth128
+					addps xmm5, CxMin128    // xmm5 = CxMin + iX*PixelWidth
+					movups _Cx, xmm5
+			}
+
+			// Apos recarregar na memoria, realiza essas operacoes em C
+			// Nao e possivel paralelizar essa parte, pois cada iteracao do loop depende da anterior
+			for (int i = 0; i < 4; i++){
+				Zx = 0;
+				Zy = 0;
+				Zx2 = 0;
+				Zy2 = 0;
+
 				for (Iteration = 0; Iteration < IterationMax && ((Zx2 + Zy2) < ER2); Iteration++)
 				{
-					Zy = 2 * Zx*Zy + _Cy[i];
-					Zx = Zx2 - Zy2 + Cx;
-					Zx2 = Zx*Zx;
-					Zy2 = Zy*Zy;
-				};
+					Zy = 2 * Zx * Zy + Cy;
+					Zx = Zx2 - Zy2 + _Cx[i];
+					Zx2 = Zx * Zx;
+					Zy2 = Zy * Zy;
+				}
+
 				/* compute  pixel color (24 bit = 3 bytes) */
 				if (Iteration == IterationMax)
 				{ /*  interior of Mandelbrot set = black */
@@ -104,17 +117,14 @@ int main()
 					color[0] = ((IterationMax - Iteration) % 8) * 63;  /* Red */
 					color[1] = ((IterationMax - Iteration) % 4) * 127;  /* Green */
 					color[2] = ((IterationMax - Iteration) % 2) * 255;  /* Blue */
-				};
+				}
+
 				/*write color to the file*/
 				fwrite(color, 1, 3, fp);
 			}
 		}
 	}
 	fclose(fp);
-
-	end = clock();
-	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;;
-	printf("%f", time_spent);
-
+	
 	return 0;
 }
--- mandelbrotSSE_iX_1.c
+++ mandelbrotSSE_iX_1.c
@@ -10,80 +10,112 @@ http://en.wikipedia.org/wiki/Image:Color_complex_plot.jpg
 create 24 bit color graphic file ,  portable pixmap file = PPM
 see http://en.wikipedia.org/wiki/Portable_pixmap
 to see the file use external application ( graphic viewer)
+-------------------------------
+3.Versao otimizada com inline assembly, com o conjunto de instrucoes SSE4_2. Essa versao
+paralelizou o loop em iX(versão preliminar 1).
 */
+
 #include <stdio.h>
 #include <math.h>
 #include <xmmintrin.h>
-#include <time.h>
 
 int main()
 {
-	// Variaveis para medir o tempo
-	clock_t begin, end;
-	double time_spent;
-
-	begin = clock();
-
 	/* screen ( integer) coordinate */
 	int iX, iY;
 	const int iXmax = 16384;
 	const int iYmax = 16384;
+
 	/* world ( double) coordinate = parameter plane*/
+	// A variavel Cx foi extendida para um vetor, para aproveitar o uso dos registradores sse
 	float _Cx[4], Cy;
 	const float CxMin = -2.5;
 	const float CxMax = 1.5;
 	const float CyMin = -2.0;
 	const float CyMax = 2.0;
-	/* */
 	float PixelWidth = (CxMax - CxMin) / iXmax;
 	float PixelHeight = (CyMax - CyMin) / iYmax;
+
 	/* color component ( R or G or B) is coded from 0 to 255 */
 	/* it is 24 bit color RGB file */
 	const int MaxColorComponentValue = 255;
 	FILE * fp;
-	char *filename = "mandelbrot.ppm";
+	char *filename = "_simd_sse_iX_1.ppm";
 	static unsigned char color[3];
+
+	// Foi criado um vetor auxiliar onde sera feita parte do calculo de color[]
 	int vetorCoresAux[4];
+
 	/* Z=Zx+Zy*i  ;   Z0 = 0 */
 	float Zx, Zy;
 	float Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
-	/*  */
+
+	// A variavel Iteration foi extendida para um vetor, para aproveitar o uso dos registradores sse
 	int Iteration[4];
 	const int IterationMax = 256;
+
 	/* bail-out value , radius of circle ;  */
 	const float EscapeRadius = 2;
 	float ER2 = EscapeRadius*EscapeRadius;
+
 	/*create new file,give it a name and open it in binary mode  */
 	fp = fopen(filename, "wb"); /* b -  binary mode */
+
 	/*write ASCII header to the file*/
 	fprintf(fp, "P6\n %d\n %d\n %d\n", iXmax, iYmax, MaxColorComponentValue);
 
-	/* compute and write image data bytes to the file*/
-
-	// Gera uma quatro palavras de 32 bits, com valor PixelWidth
-	// Funcao intrinseca
+	// Gera uma quatro palavras de 32 bits, com valor PixelWidth por meio de funcoes intrinsecas
 	__m128 PixelWidth128 = _mm_set1_ps(PixelWidth);
 	__m128 CxMin128 = _mm_set1_ps(CxMin);
 	__m128 IterationMax128 = _mm_set1_ps(IterationMax);
 
+	// Float utilizado para multiplicar os valores em simdIx
+	float valor4 = 4.0;
+	int counter; // counter utilizado com auxiliar no calculo de simdIx
+
+	/* compute and write image data bytes to the file*/
 	for (iY = 0; iY<iYmax; iY++)
 	{
 		Cy = CyMin + iY*PixelHeight;
 
 		if (fabs(Cy)< PixelHeight / 2) Cy = 0.0; /* Main antenna */
+
+		// Loop paralelizado(são feitas 4 iteracoes simultaneamente)
 		for (iX = 0; iX<iXmax / 4; iX++)
 		{
+
 			float simdIx[4];
-			for (int i = 0; i < 4; i++)
-				simdIx[i] = iX * 4.0 + i;
 
 			_asm{
+					finit
+					mov ecx, 0
+
+					// Gera o vetor simdIx
+				L1:
+					fild iX
+					fld valor4
+					fmul
+					mov counter, ecx
+					fild counter
+					fadd
+					fstp simdIx[ecx]
+
+					add ecx, 4
+					cmp ecx, 16
+					je FIM
+					jmp L1
+
+				FIM :
+
+				// xmm5 = CxMin + iX*PixelWidth
 				movups xmm5, simdIx
 					mulps xmm5, PixelWidth128
-					addps xmm5, CxMin128    // xmm5 = CxMin + iX*PixelWidth
+					addps xmm5, CxMin128
 					movups _Cx, xmm5
 			}
 
+			// Apos recarregar na memoria, realiza essas operacoes em C
+			// Nao e possivel paralelizar essa parte, pois cada iteracao do loop depende da anterior
 			for (int i = 0; i < 4; i++){
 				Zx = 0;
 				Zy = 0;
@@ -99,13 +131,14 @@ int main()
 				}
 			}
 
-
+			// Volta para asm, realizando apenas parte do calculo
 			_asm{
+				// xmm1 = color[0], xmm2 = color[1], xmm3 = color[2]
 				movups xmm0, Iteration
 					movups xmm4, IterationMax128
 					movups xmm5, xmm0
 
-					xorps xmm1, xmm1	// xmm1 = color[0], xmm2 = color[1], xmm3 = color[2]
+					xorps xmm1, xmm1
 					xorps xmm2, xmm2
 					xorps xmm3, xmm3
 
@@ -129,10 +162,6 @@ int main()
 		}
 	}
 	fclose(fp);
-
-	end = clock();
-	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;;
-	printf("%f", time_spent);
 
 	return 0;
 }
